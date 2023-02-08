f1ff663468dae5f1ed4007c7a6c3940c82b76689
Subodha Sahu <subodha.sahu91@gmail.com>
2023-02-08 11:35:30 +0530

    API Changes

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -7,12 +7,12 @@ const jsonFile = path.join(process.cwd(), 'json') + '/events.json';
 
 
 router.get("/data", async (req, res) => {
-    //Find the absolute path of the json directory
-  const jsonDirectory = path.join(process.cwd(), 'json');
-  //Read the json data file data.json
-  const fileContents = await fs.readFile(jsonDirectory + '/data.json');
-  //Return the content of the data file in json format
-  res.status(200).json(fileContents);
+        //Find the absolute path of the json directory
+        const jsonDirectory = path.join(process.cwd(), "json");
+        //Read the json data file data.json
+        const fileContents = await fs.readFile(jsonDirectory + "/data.json");
+        //Return the content of the data file in json format
+        res.status(200).json(fileContents);
 });
 
 router.get("/", async (req, res) => {
