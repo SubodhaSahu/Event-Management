22c0b23610d97d06027a6a370b2e7efb828353bf
Subodha Sahu <subodha.sahu91@gmail.com>
2023-02-08 11:07:33 +0530

    Check the code from the vercel app

--- backend/routes/event.js
+++ backend/routes/event.js
@@ -5,6 +5,16 @@ const router = express.Router();
 
 const jsonFile = path.join(process.cwd(), 'json') + '/events.json';
 
+
+router.get("/data", async (req, res) => {
+    //Find the absolute path of the json directory
+  const jsonDirectory = path.join(process.cwd(), 'json');
+  //Read the json data file data.json
+  const fileContents = await fs.readFile(jsonDirectory + '/data.json');
+  //Return the content of the data file in json format
+  res.status(200).json(fileContents);
+});
+
 router.get("/", async (req, res) => {
     fs.readFile(jsonFile)
         .then((data) => {
