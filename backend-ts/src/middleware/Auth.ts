e948125ae9a7afde92eb9710956db0bcfddabbdf
Subodha Sahu <subodha.sahu91@gmail.com>
2023-03-09 20:37:18 +0530

    Git Ignore file

--- backend-ts/src/middleware/Auth.ts
+++ backend-ts/src/middleware/Auth.ts
@@ -7,9 +7,9 @@ export const AuthMiddleWare = async (req : Request, res: Response, next :NextFun
     try {
         const { authorization } = req.headers;
         if (authorization) {
-            const decodedToken = jwt.verify(authorization, config.SECRET_KEY)
-            next();
+            const decodedToken = await jwt.verify(authorization, config.SECRET_KEY);
             Logging.info('Middle ware for validation');    
+            next();       
         } else {
             Logging.error('Auth Token required in the header');
             return res.status(HttpCode.UNAUTHORIZED).json('Auth Token required in the header as Authorization ');
